35ec875978295eb4b4cdf98560fddbd20f9679da
Joseph Ghobrial <josephghobrial77@gmail.com>
2025-01-22 13:07:07 -0500

    Assignment Added

---
+++ COMP100/C# Coding/Assignment/IPO Charts and Algorithms.cs
@@ -0,0 +1,22 @@
+﻿// Input
+double numberOfScoops;
+double pricePerScoop;
+
+// Output
+double totalCost;
+
+// Processing
+// no variable needed
+
+// Algorithm
+Console.Write("Enter number of scoops added: ");
+
+numberOfScoops = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter price per scoop: $");
+
+pricePerScoop = Convert.ToDouble(Console.ReadLine());
+
+totalCost = numberOfScoops * pricePerScoop;
+
+Console.WriteLine("Total cost of ice-cream for a customer is: $" + totalCost);
