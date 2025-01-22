5c52db78b8eaae005892cf7de9bde3a27db7c876
Joseph Ghobrial <josephghobrial77@gmail.com>
2025-01-22 15:09:16 -0500

    Assignment Added

---
+++ COMP100/FirstCSharpProject/IPO Chars and Alogrithms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+// Input
+double numberOfScoops;
+double pricePerScoop;
+
+// Output       
+double totalCost;
+
+// Processing
+// no variable needed
+
+// Algorithm
+Console.Write("Enter number of scoops added: ");
+
+numberOfScoops = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter price per scoop: $");
+
+pricePerScoop = Convert.ToDouble(Console.ReadLine());
+
+totalCost = numberOfScoops * pricePerScoop;
+
+Console.WriteLine("Total cost of ice-cream for a customer is: $" + totalCost);
+    }
+}
